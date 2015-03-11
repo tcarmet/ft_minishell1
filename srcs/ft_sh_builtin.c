cda2aebaae6a1d2bafc31b584fbc063c0a85749a
Thomas Carmet <tcarmet@MacBook-Pro-de-Thomas.local>
2015-03-11 23:28:40 +0100

    OH LA LA j'envois du lourd! une ligne de code pour exit

--- srcs/ft_sh_builtin.c
+++ srcs/ft_sh_builtin.c
@@ -36,8 +36,8 @@ void	ft_exec_builtin(char **str, t_all *all)
 		ft_sh_setenv(str, all);
 	else if (ft_strequ("unsetenv", str[0]))
 		ft_sh_unsetenv(str, all);
-	// else if (ft_strequ("exit", str))
-	// 	return (1);
+	else if (ft_strequ("exit", str[0]
+		exit(0);
 	// else if (ft_strequ("cd", str))
 	// 	ft_sh_cd();
 }
