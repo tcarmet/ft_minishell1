085fbe7bea4a905a359e25cdad2af429a656dd97
tcarmet <tcarmet@hotmail.com>
2015-03-14 15:37:08 +0100

    ft_strjoint correction

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -5,39 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tcarmet <tcarmet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/11/11 22:38:57 by tcarmet           #+#    #+#             */
-/*   Updated: 2014/11/23 18:00:23 by tcarmet          ###   ########.fr       */
+/*   Created: 2015/03/14 15:34:17 by tcarmet           #+#    #+#             */
+/*   Updated: 2015/03/14 15:35:13 by tcarmet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char		*ft_strjoin(char const *s1, char const *s2)
+char			*ft_strjoin(char const *s1, char const *s2)
 {
-	int		len;
-	int		n;
-	int		counter;
-	char	*final;
+	char			*new;
 
-	if (s1 == NULL && s2 == NULL)
-		return ((char *)NULL);
-	if (s1 == NULL && s2)
-		return (ft_strdup(s2));
-	if (s2 == NULL && s1)
-		return (ft_strdup(s1));
-	counter = 0;
-	len = ft_strlen(s1);
-	n = len + ft_strlen(s2) + 1;
-	if (!(final = ft_strnew(n)))
-		return (NULL);
-	while (counter < n)
+	if (s1 && s2)
 	{
-		if (counter < len)
-			final[counter] = s1[counter];
-		else
-			final[counter] = s2[counter - len];
-		counter++;
+		new = ft_strnew(ft_strlen(s1) + ft_strlen(s2) + 1);
+		if (new)
+		{
+			new = ft_strcat(new, s1);
+			new = ft_strcat(new, s2);
+			return (new);
+		}
 	}
-	final[counter] = '\0';
-	return (final);
+	return (NULL);
 }
