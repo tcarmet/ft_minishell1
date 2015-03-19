a82666556fe98273316e575c12879092aa48f1e4
tcarmet <tcarmet@hotmail.com>
2015-03-19 11:54:33 +0100

    commit number 100 IS MINE MWHAHAHA, bug with env -i solved

--- srcs/ft_sh_init.c
+++ srcs/ft_sh_init.c
@@ -29,17 +29,19 @@ int		ft_sh_check_env(char **env, t_all *all)
 	tmp = NULL;
 	if (env[0])
 		return (1);
-	str = (char **)malloc(sizeof(char *) * 3);
+	str = (char **)malloc(sizeof(char *) * 4);
 	str[0] = ft_strdup("setenv");
 	str[1] = ft_strdup("PWD");
 	str[2] = getcwd(tmp, 1024);
+	str[3] = NULL;
 	all->home = ft_strdup(str[2]);
 	ft_sh_setenv(str, all);
 	free_tb(&str);
-	str = (char **)malloc(sizeof(char *) * 3);
+	str = (char **)malloc(sizeof(char *) * 4);
 	str[0] = ft_strdup("setenv");
 	str[1] = ft_strdup("SHLVL");
 	str[2] = ft_strdup("1");
+	str[3] = NULL;
 	ft_sh_setenv(str, all);
 	free_tb(&str);
 	return (0);
